b79647253e93ce81631b7fa8d559ad7cedcca106
Yohanes Senbeto <121575345+YohanesSenbeto@users.noreply.github.com>
2025-08-18 10:59:36 +0300

    Changed home page button color

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,10 +8,7 @@ const NavBar = () => {
 
     return (
         <div className="flex mr-5 py-4 px-5 bg-slate-200">
-            <Link
-                href="/"
-                className="p-2 px-4 bg-blue-500 text-white rounded-md"
-            >
+            <Link href="/" className="p-2 px-4 text-white rounded-md">
                 Home
             </Link>
             <Link href="/users" className="p-2 px-4 mx-4">
